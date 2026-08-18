eb9f6c98617551e60fef77b52ea7e6ba7fcb7c2f
bkhitpbd-commits <bkhit.pbd@gmail.com>
2026-08-18 13:45:07 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: process.env.GITHUB_ACTIONS ? '/Form-Inputan-Web-/' : '/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
